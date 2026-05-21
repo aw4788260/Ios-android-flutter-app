cedc282f92ba8425792841afc07ccdc50857093d
medaad5682 <medaad5682@gmail.com>
2026-05-22 02:53:25 +0300

    Update subject_materials_screen.dart

--- lib/presentation/screens/subject_materials_screen.dart
+++ lib/presentation/screens/subject_materials_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import '../../core/constants/app_colors.dart';
 import '../../core/services/app_state.dart';
