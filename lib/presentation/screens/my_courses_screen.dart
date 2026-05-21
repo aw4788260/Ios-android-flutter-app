daf9ae446af2e9285d4dc36ae0a1593f8123ee34
medaad5682 <medaad5682@gmail.com>
2026-05-22 02:47:49 +0300

    Update my_courses_screen.dart

--- lib/presentation/screens/my_courses_screen.dart
+++ lib/presentation/screens/my_courses_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import '../../core/constants/app_colors.dart';
 import '../../core/services/app_state.dart';
 import '../../core/services/storage_service.dart';
